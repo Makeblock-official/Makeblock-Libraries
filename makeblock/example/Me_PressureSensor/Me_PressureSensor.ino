70874c8a5249f2492f7f0f9591eb44ae4f16060e
indream <flashindream@gmail.com>
2016-04-20 12:04:13 +0800

    add bmp180 pressure sensor

---
+++ makeblock/example/Me_PressureSensor/Me_PressureSensor.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include <MeMegaPi.h>
+
+MePressureSensor bmp;
+  
+void setup() {
+  Serial.begin(115200);
+  if (!bmp.begin()) {
+  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  while (1) {}
+  }
+}
+  
+void loop() {
+    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" ℃");
+    
+    Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+    
+    Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+
+    Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.println(" meters");
+    
+    Serial.println();
+    delay(2000);
+}
