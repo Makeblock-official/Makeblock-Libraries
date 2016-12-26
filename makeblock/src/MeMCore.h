1b672624d8c1f37b743a8ddd54ce72e13f49b556
Bin_Go <296152377@qq.com>
2016-12-26 12:22:28 +0800

    add examples for pm2.5sensor

--- makeblock/src/MeMCore.h
+++ makeblock/src/MeMCore.h
@@ -31,6 +31,7 @@
  * Scott wang       2016/09/18         1.0.2            Add the PORT[15].
  * Scott            2016/09/20         1.0.3            Add the PORT[16].
  * Scott            2016/09/23         1.0.4            Add the MePS2.h .
+ * Zzipeng          2016/12/15         1.0.5            Add the MePm25Sensor.h .
  * </pre>
  */
 #ifndef MeMCore_H
@@ -72,6 +73,7 @@
 #include "MeFlameSensor.h"
 #include "MeGasSensor.h"
 #include "MePS2.h"
+#include "MePm25Sensor.h"
 
 /*********************  Mbot Board GPIO Map *********************************/
 MePort_Sig mePort[17] =
